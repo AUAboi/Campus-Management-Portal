e5d0e6aba8d0442a7544f2b12ef7bfb9bc1c7369
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2021-10-15 22:27:49 +0500

    inertia js setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <script>
+      let 
+    </script>
   </head>
   <body>
     @inertia
